62f04954b610b02e69d44a4d91a46ca900a46a78
leo1203765 <96751949+leo1203765@users.noreply.github.com>
2021-12-27 16:54:06 -0600

    Add files via upload

---
+++ PROC9_P5Juego-main/PROC9_P5Juego-main/sketch.js
@@ -0,0 +1,28 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+  
+
+}
+
+
+function draw() 
+{
+  background(34,251,233);
+
+  if (keyIsDown(RIGHT_ARROW))
+  {
+    box.position.x = box.position.x +5;
+  }
+
+  if (keyIsDown(LEFT_ARROW))
+  {
+    box.position.x = box.position.x -5;
+  }
+drawSprites();
+}
+
+
+
+
